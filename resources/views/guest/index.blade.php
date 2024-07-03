a1405dbdfa99ba360e2bf5ea763a3d75f22d822b
febby212 <febbyweng@gmail.com>
2024-07-03 21:26:40 +0700

    minor fix in guest page

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -8,11 +8,11 @@
     <!-- Carousel Start -->
     <div class="container-fluid px-0">
         <div id="carouselId" class="carousel slide" data-bs-ride="carousel">
-            <ol class="carousel-indicators">
+            {{-- <ol class="carousel-indicators">
                 <li data-bs-target="#carouselId" data-bs-slide-to="0" class="active" aria-current="true"
                     aria-label="First slide"></li>
                 <li data-bs-target="#carouselId" data-bs-slide-to="1" aria-label="Second slide"></li>
-            </ol>
+            </ol> --}}
             <div class="carousel-inner" role="listbox">
                 <div class="carousel-item active">
                     <img src="assetsUsers/img/carousel-1.jpg" class="img-fluid" alt="First slide">
@@ -46,14 +46,6 @@
                     </div>
                 </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselId" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselId" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
         </div>
     </div>
     <!-- Carousel End -->
